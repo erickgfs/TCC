e17ff8ce11e89a526e39f4698194bb43a260f34f
erickgfs <erickgfs@gmail.com>
2021-08-25 23:18:22 -0300

    ajustes

--- FrontEnd/tcc/src/pages/Registration/index.tsx
+++ FrontEnd/tcc/src/pages/Registration/index.tsx
@@ -1,11 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Form } from '@unform/web';
+import {
+  FiUser,
+  FiEdit3,
+  FiMap,
+  FiBookmark,
+  FiArrowLeft,
+} from 'react-icons/fi';
 
-import { Container } from './styled';
+import {
+  Container,
+  FormContent,
+  EstadosContainer,
+  VisitMunicipiosDiv,
+} from './styled';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const Registration: React.FC = () => {
+  const [visitMunicipios, setVisitMunicipios] = useState<any>([]);
+
+  function handleSubmit(data: object): void {
+    console.log(data);
+  }
+
+  const addVisitMunicipios = (e: any) => {
+    e.preventDefault();
+
+    setVisitMunicipios([...visitMunicipios, '']);
+  };
+
+  const removeVisitMunicipios = (e: any) => {
+    e.preventDefault();
+
+    setVisitMunicipios(visitMunicipios.splice(1, visitMunicipios.length));
+  };
+
   return (
     <Container>
-      <h1>teste</h1>
+      <Form onSubmit={handleSubmit}>
+        <h1>Cadastro de Paciente</h1>
+        <FormContent>
+          <div>
+            <Input name="nome" icon={FiUser} placeholder="Nome"></Input>
+            <Input name="cpf" icon={FiEdit3} placeholder="CPF"></Input>
+          </div>
+          <div>
+            <Input
+              name="municipio"
+              icon={FiMap}
+              placeholder="Município"
+            ></Input>
+            <Input
+              name="dataNascimento"
+              icon={FiBookmark}
+              placeholder="Data de Nascimento"
+            ></Input>
+          </div>
+          <EstadosContainer>
+            <h2>Visitou outro município?</h2>
+            <Button type="button" onClick={addVisitMunicipios}>
+              +
+            </Button>
+            <Button type="button" onClick={removeVisitMunicipios}>
+              -
+            </Button>
+          </EstadosContainer>
+          <VisitMunicipiosDiv>
+            {visitMunicipios.map((municipio: any, index: any) => (
+              <Input
+                name="visitMunicipio"
+                icon={FiUser}
+                placeholder="Município"
+              ></Input>
+            ))}
+          </VisitMunicipiosDiv>
+          <Button type="submit">Cadastrar</Button>
+        </FormContent>
+      </Form>
+      <a href="/search">
+        <FiArrowLeft />
+        Voltar
+      </a>
     </Container>
   );
 };
--- FrontEnd/tcc/src/pages/Registration/styled.ts
+++ FrontEnd/tcc/src/pages/Registration/styled.ts
@@ -1,3 +1,58 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h1 {
+    text-align: center;
+  }
+
+  button {
+    align-self: center;
+  }
+`;
+
+export const FormContent = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div {
+    display: flex;
+    place-self: center;
+    margin: 5px;
+
+    div {
+      align-items: center;
+    }
+  }
+`;
+
+export const VisitMunicipiosDiv = styled.div`
+  display: flex;
+  place-content: center;
+  flex-wrap: wrap;
+`;
+
+export const EstadosContainer = styled.div`
+  display: flex;
+  justify-content: center;
+
+  button {
+    margin: 0 0 0 10px;
+    padding: 5px 10px;
+    font-size: 16px;
+  }
+
+  button + button {
+    background-color: red;
+    padding: 3px 12px;
+    font-size: 20px;
+
+    &:hover {
+      background-color: ${shade(0.2, 'red')};
+    }
+  }
+`;
