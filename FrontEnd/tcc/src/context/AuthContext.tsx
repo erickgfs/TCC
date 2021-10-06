495328c7da4eca79dd7d3c10e60f7ca3755a9aec
erickgfs <erickgfs@gmail.com>
2021-10-06 00:36:36 -0300

    ajustes

--- FrontEnd/tcc/src/context/AuthContext.tsx
+++ FrontEnd/tcc/src/context/AuthContext.tsx
@@ -1,5 +1,11 @@
-import React, { createContext, useCallback } from 'react';
+import React, { createContext, useCallback, useState } from 'react';
+import jwt_decode from 'jwt-decode';
 import api from '../services/api';
+
+interface AuthState {
+  token: string;
+  user_type: string;
+}
 
 interface LoginCredentials {
   email: string;
@@ -16,9 +22,28 @@ export const AuthContext = createContext<AuthContextData>(
 );
 
 export const AuthProvider: React.FC = ({ children }) => {
+  const [data, setData] = useState<AuthState>(() => {
+    const token = localStorage.getItem('@TCC:token');
+    const user_type = localStorage.getItem('@TCC:user_type');
+
+    if (token && user_type) {
+      return { token, user_type };
+    }
+
+    return {} as AuthState;
+  });
+
   const login = useCallback(async ({ email, password }) => {
     const response = await api.post('/auth', { email, password });
-    console.log('teste');
+    const tokenDecode: any = jwt_decode(response.data.token);
+
+    const { token } = response.data.token;
+    const { user_type } = tokenDecode.user_type;
+
+    localStorage.setItem('@TCC:token', token);
+    localStorage.setItem('@TCC:user_type', user_type);
+
+    setData({ token, user_type });
   }, []);
 
   return (
